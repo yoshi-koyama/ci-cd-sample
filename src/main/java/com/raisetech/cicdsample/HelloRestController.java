5365883372231b4ad500435b7fb8961797cc363f
小山由人 <koyama@reytech.co.jp>
2022-04-08 14:57:21 +0900

    helloエンドポイントとテストを追加

---
+++ src/main/java/com/raisetech/cicdsample/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.raisetech.cicdsample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
--- src/test/java/com/raisetech/cicdsample/CiCdSampleApplicationTests.java
+++ src/test/java/com/raisetech/cicdsample/CiCdSampleApplicationTests.java
@@ -1,13 +1,25 @@
 package com.raisetech.cicdsample;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CiCdSampleApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void helloエンドポイントにアクセスすると200ステータスがhelloという文字列が返されること() throws Exception {
+		mockMvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string("hello"));
 	}
 
 }
